f023fc4494be1bc1eb3419006ce178e8e2086f87
tracht <marija.trachtenberg@gmail.com>
2020-06-16 18:04:46 +0100

    creating API fetch request in TweetsComponent

--- src/TweetsComponent.js
+++ src/TweetsComponent.js
@@ -5,17 +5,32 @@ class Tweets extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      data: null,
+    };
+  }
 
-  },
+  componentDidMount() {
+    fetch('https://chitter-backend-api-v2.herokuapp.com/peeps')
+    .then(response => response.json())
+    .then(data => this.setState({ data }));
+  }
 
   render() {
     
-    return <div>
-      <h1> Hello there! </h1>
+    const { data } = this.state;
 
-    </div>;
+    return ( <div>
+     <h1> Hello there! </h1>
+
+     <p> {data.body} </p>
+     <p> {data.user.handle} posted on {data.updated_at} </p>
+
+    </div>
     
+    )
   }
+
 }
 
 export default Tweets;
