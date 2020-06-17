4c2b75e1812bdba8c0445d7368047b4bcd73bc82
tracht <marija.trachtenberg@gmail.com>
2020-06-17 14:01:39 +0100

    Welcome to Twitter is centered & has grey background

--- src/HomepageComponent.js
+++ src/HomepageComponent.js
@@ -5,7 +5,7 @@ class Homepage extends React.Component{
 
   render() {
     return <div>
-        <h2>
+        <h2 class="homepage-header">
           <img src="https://bit.ly/37wXEkG" alt="red angry bird" float="left" width="30" height="50"/>
           Welcome to Twitter 
         </h2>
