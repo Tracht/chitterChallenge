624aff73f595b20e714586f12fc18607d005d7f8
tracht <marija.trachtenberg@gmail.com>
2020-06-18 15:21:30 +0100

    passes test: user signs up with unique handle, already registered handle

--- cypress/integration/sign_up_spec.js
+++ cypress/integration/sign_up_spec.js
@@ -1,9 +1,19 @@
 describe('sign up', () => {
   it('with unique handle & password is successful', () => {
     cy.visit('http://localhost:3000')
-    cy.get('.signup-form').find('[id="sign-up-handle"]').type('catLover_meow')
+    cy.get('.signup-form').find('[id="sign-up-handle"]').type('SweetBabyJesus')
     cy.get('.signup-form').find('[id="sign-up-password"]').type('12345')
     cy.get('.signup-form').submit();
-    cy.get('.message').should('contain', 'Sign up successful.')
+    cy.get('.message').should('contain', 'Your sign up was successful.')
+  })
+})
+
+describe('sign up', () => {
+  it('with already registered handle', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.signup-form').find('[id="sign-up-handle"]').type('SweetBabyJesus')
+    cy.get('.signup-form').find('[id="sign-up-password"]').type('apple')
+    cy.get('.signup-form').submit();
+    cy.get('.message').should('contain', 'This handle is already registered, pick another.')
   })
 })
--- src/UserComponent.js
+++ src/UserComponent.js
@@ -3,40 +3,86 @@ import './index.css'
 
 class User extends React.Component {
 
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      message: "To start tweeting, please login or signup."
+      message: "To start tweeting, please login or signup.",
+      user: {
+        handle: "",
+        id: ""
+      }
     }
-  }
+  };
 
-  componentDidMount() {
-    this.signup('https://chitter-backend-api-v2.herokuapp.com/users')
-  }
-
-  signup = (postData) => {
+  signupHandler = (event) => {
+    event.preventDefault();
+    let newSignup = {
+      user: {
+        handle: this.state.handle,
+        password: this.state.password
+      }
+    }
     
+    // console.log("Sign Up Handler 1");
+    // console.log(newSignup);
+    // console.log(this.state.message);
+    // console.log("Sign Up Handler 2");
+
+    fetch('https://chitter-backend-api-v2.herokuapp.com/users', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json'}, 
+      body: JSON.stringify(newSignup),
+    })
+    .then(response => response.json())
+    .then((result) => {
+      if (result.handle[0] === "has already been taken") {
+        this.setState({ 
+          user: { id: "", handle: "" },
+          message:  "This handle is already registered, pick another."
+         })
+      } else {
+        // console.log(result);
+        // console.log(result.handle);
+        // console.log(result.id);
+        this.setState({ 
+          user: { id: result.id, handle: result.handle },
+          message: `Your sign up was successful.`
+       })
+      }
+      console.log(this.state.message);
+    });
+
+    // console.log("Post fetch")
+    // console.log(this.state.message)
+
+    // this.cancelCourse()
+  }
+
+  cancelCourse = () => {
+    document.getElementsByClassName("signup-form").reset();
+  }
+
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
-    return ( 
+   
+    const { handle, password, message } = this.state;
 
-    <div class="user-position">
-      
-      <section> 
-        <form action="" class="signup-form">
-          <input type="text" id="sign-up-handle" name="handle" placeholder="pick a handle"></input>
-          <input type="text" id="sign-up-password" name="handle" placeholder="pick a password"></input>
+    return ( 
+      <div class="user-position">
+        <p class="message"> { message } </p>
+        <form action="post" class="signup-form" onSubmit={this.signupHandler}>
+          <input onChange={this.onChange} type="text" id="sign-up-handle" name="handle" value={handle} placeholder="pick a handle" ></input>
+          <input onChange={this.onChange} type="text" id="sign-up-password" name="password" value={password}placeholder="pick a password"></input>
           <input type="submit" value="Sign up"></input>
         </form>
-      </section>
-
-    </div>
-
-    )
-   
+      </div>
+    );
   }
-
 
 }
 
